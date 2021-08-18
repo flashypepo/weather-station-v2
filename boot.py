836551cb1156e23e4a70b4aa7112e3d7563baeae
flashypepo <peter@pepo.nl>
2021-08-18 17:57:18 +0200

    update boot.py

--- boot.py
+++ boot.py
@@ -5,6 +5,8 @@ micropython.alloc_emergency_exception_buf(100)
 #import esp
 #esp.osdebug(None)
 #import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
 #import webrepl
 #webrepl.start()
 
